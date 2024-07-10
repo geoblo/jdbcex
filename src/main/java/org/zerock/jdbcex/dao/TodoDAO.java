bdc1ec25f90011d281efbc816098c20bccf7cf49
geoblo <geoblo@naver.com>
2024-07-10 16:33:26 +0900

    7/10 1st

---
+++ src/main/java/org/zerock/jdbcex/dao/TodoDAO.java
@@ -0,0 +1,45 @@
+package org.zerock.jdbcex.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TodoDAO {
+
+    public String getTime() {
+        String now = null;
+
+        try (
+            Connection connection = ConnectionUtil.INSTANCE.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+            ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
+            resultSet.next();
+
+            now = resultSet.getString(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return now;
+    }
+
+    public String getTime2() {
+        String now = null;
+
+        try (
+                Connection connection = ConnectionUtil.INSTANCE.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+                ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
+            resultSet.next();
+
+            now = resultSet.getString(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return now;
+    }
+}
